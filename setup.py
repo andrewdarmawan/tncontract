1dd5b05453f8f48a94b032743ea224c32d32cba0
Arne Grimsmo <arne.grimsmo@gmail.com>
2016-09-17 19:57:37 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "tncontract",
+    version = "0.1.0",
+    packages = find_packages(),
+    author = "Andrew Darmawan",
+    license = "MIT",
+    install_requires = ["numpy", "scipy"],
+)
