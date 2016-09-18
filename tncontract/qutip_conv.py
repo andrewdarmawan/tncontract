08069ebdeeae5bd7c89c67393b72c233a1c0e13f
Arne Grimsmo <arne.grimsmo@gmail.com>
2016-09-18 16:36:20 -0400

    Add functions for creating MPS/MPO
    qobjlist_to_mpo() and qobjlist_to_mps() creates an MPO/MPS from a list
    of qutip Qobj's. Many-body Qobj's are broken up into MPO/MPS form by
    exact SVD.

--- tncontract/qutip_conv.py
+++ tncontract/qutip_conv.py
@@ -13,7 +13,7 @@ import qutip as qt
 import tncontract as tn
 
 
-def qobj_to_tensor(qobj, labels=None):
+def qobj_to_tensor(qobj, labels=None, trim_dummy=True):
     """
     Convert a `qutip.Qobj` object to a `Tensor`
 
@@ -24,10 +24,8 @@ def qobj_to_tensor(qobj, labels=None):
     labels : list, optional
         List of labels for the indices. Output labels followed by input labels.
         Defaults to `['out1', ..., 'outk', 'in1', ..., 'ink']
-
-    Notes
-    -----
-    Dummy indices of dimension one will be trimmed away.
+    trim_dummy : bool
+        If true dummy indices of dimension one are trimmed away
     """
 
     data = qobj.data.toarray()
@@ -45,10 +43,10 @@ def qobj_to_tensor(qobj, labels=None):
     else:
         output_labels = labels[:nsys]
         input_labels = labels[nsys:]
-
     t = tn.matrix_to_tensor(data, output_dims, input_dims, output_labels,
             input_labels)
-    t.remove_all_dummy_indices()
+    if trim_dummy:
+        t.remove_all_dummy_indices()
     return t
 
 
@@ -77,3 +75,58 @@ def tensor_to_qobj(tensor, output_labels):
     dims = [output_dims, input_dims]
     return qt.Qobj(data, dims=dims)
 
+
+def qobjlist_to_mpo(qobjlist):
+    """
+    Construct an MPO from a list of Qobj operators.
+
+    Many-body operators are put in MPO form by exact SVD, and virtual "left"
+    and "right" indices with bond dimension one are added between the elements
+    of the list.
+    """
+    tensors = []
+    for i, qobj in enumerate(qobjlist):
+        if not len(np.shape(qobj.dims)) == 2:
+            # wrong dims (not a ket, bra or operator)
+            raise ValueError("qobj element not a ket/bra/operator")
+
+        nsys = len(qobj.dims[0])
+        t = qobj_to_tensor(qobj, trim_dummy=False)
+
+        # Add left and right indices with bonddim one
+        t.add_dummy_index('left', -1)
+        t.add_dummy_index('right', -1)
+
+        # Break up many-body operators by SVDing
+        for k in range(nsys-1):
+            U, S, V = tn.tensor_svd(t, ['out'+str(k), 'in'+str(k), 'left'])
+            U.replace_label('svd_in', 'right')
+            U.replace_label('out'+str(k), 'physout')
+            U.replace_label('in'+str(k), 'physin')
+            tensors.append(U)
+            t = tn.contract(S, V, ['svd_in'], ['svd_out'])
+            t.replace_label('svd_out', 'left')
+        t.replace_label('out'+str(nsys-1), 'physout')
+        t.replace_label('in'+str(nsys-1), 'physin')
+        tensors.append(t)
+    return tn.MatrixProductOperator(tensors, left_label='left',
+        right_label='right', physin_label='physin', physout_label='physout')
+
+
+def qobjlist_to_mps(qobjlist):
+    """
+    Construct an MPS from a list of Qobj kets.
+
+    Many-body states are put in MPS form by exact SVD, and virtual "left"
+    and "right" indices with bond dimension one are added between the elements
+    of the list.
+    """
+    mpo = qobjlist_to_mpo(qobjlist)
+    tensors = mpo.data
+    for t in tensors:
+        # Remove dummy input labels
+        t.remove_all_dummy_indices(labels=['physin'])
+        # Change physical label to the standard choice 'phys'
+        t.replace_label(['physout'], ['phys'])
+    return tn.MatrixProductState(tensors, left_label='left',
+        right_label='right', phys_label='phys')
