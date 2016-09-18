1595967c35c960b41e13e33a7aa014a22bd2e740
Arne Grimsmo <arne.grimsmo@gmail.com>
2016-09-18 00:36:58 -0400

    Add version control
    Version controlled from tncontract/version.py. Read by setup.py and
    tncontract/__init__py.

--- tncontract/__init__.py
+++ tncontract/__init__.py
@@ -1,7 +1,4 @@
+from tncontract.version import __version__
 from tncontract.tensor import *
 from tncontract.one_dimension import *
 from tncontract.square_lattice import *
-
-#import tncontract.tensor 
-#import tncontract.one_dimension 
-#import tncontract.square_lattice 
---
+++ tncontract/version.py
@@ -0,0 +1,2 @@
+# Package version. Loaded by setup.py and __init__.py.
+__version__ = '0.1.0'
