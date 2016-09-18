1dc0c87051f829404198690e42cfc00f181dc6db
Arne Grimsmo <arne.grimsmo@gmail.com>
2016-09-18 00:36:58 -0400

    Add version control
    Version controlled from tncontract/version.py. Read by setup.py and
    tncontract/__init__py.

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
 
+# Get version from tncontract/version.py
+exec(open("tncontract/version.py").read())
 
 setup(
     name = "tncontract",
-    version = "0.1.0",
+    version = __version__,
     packages = find_packages(),
     author = "Andrew Darmawan",
     license = "MIT",
